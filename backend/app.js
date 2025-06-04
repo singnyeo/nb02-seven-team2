b754c7b25c40ff6c2e5681da4c3b10de798cf9e4
Kang si yeon <rkdtldus0115@naver.com>
2025-06-04 14:04:52 +0900

    chore: app.js 초기 설정 및 서버 실행 코드 작성

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+  res.send('서버가 정상적으로 실행 중');
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('서버 에러 발생!');
+});
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`서버 실행 중 http://localhost:${PORT}`);
+});
