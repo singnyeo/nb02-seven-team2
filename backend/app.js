ee345d1724903f2b475d25466f08431565549644
jrkgus413 <jrkgus413@naver.com>
2025-06-12 09:43:23 +0900

    chore: app.js / group.js 충돌 머지

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 const errorHandler = require('./middlewares/error-handler');
 
 const app = express();
+const groupRoutes = require('./routes/groups');
 const { STATUS_CODE } = require('./utils/const');
 
 // 기본 미들웨어 설정
@@ -14,6 +15,7 @@ app.use(cors());
 app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use('/groups', groupRoutes);
 
 // 라우터 연결
 const groupRoutes = require('./routes/groups');
