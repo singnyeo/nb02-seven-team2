b3a078bf085a54439ae75c2398a3e18310c6698e
sooyen_cha <cktndus32@naver.com>
2025-06-12 13:37:21 +0900

    fix: 충돌 헤결

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -1,22 +1,37 @@
 const express = require('express');
-const GroupController = require('../controllers/GroupController');
+const groupController = require('../controllers/group-controller');
 
 const router = express.Router();
 
 /**
  * 그룹 목록 조회 API
- * GET /api/groups
+ * GET /groups
  */
-// router.get('/', GroupController.getGroups);
+router.get('/', groupController.getGroups);
 
 /**
  * 그룹 상세 조회 API
- * GET /api/groups/:id
+ * GET /groups/:groupId
  */
-// router.get('/:id', GroupController.getGroupById);
+router.get('/:groupId', groupController.getGroupById);
 
+/** 
+ * 그룹 생성 API
+ * POST /groups
+ */
 router.post('/', GroupController.postGroup);
+
+/**
+ * 그룹 수정 API
+ * PAtCH /groups/:id
+ */
 router.patch('/:id', GroupController.patchGroup);
+
+/**
+ * 그룹 삭제 API
+ * EELETE /groups/:id
+ */
 router.delete('/:id', GroupController.deleteGroup);
 
 module.exports = router;
+
