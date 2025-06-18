b17746d79c785e3c0e779e0d120e99e879461eb3
jrkgus413 <jrkgus413@naver.com>
2025-06-18 10:52:54 +0900

    fix: 그룹 참여 생성 및 취소 로직 개선, 주석 정리

--- backend/controllers/group-participant-controller.js
+++ backend/controllers/group-participant-controller.js
@@ -1,7 +1,6 @@
 const db = require('../utils/db');
 const { ERROR_MSG, STATUS_CODE } = require('../utils/const');
 const handleError = require('../utils/error');
-const { ta } = require('date-fns/locale');
 
 /** 유저 조회 */
 const findUserByNickname = (nickname) => db.user.findUnique({ where: { nickname } });
@@ -30,20 +29,20 @@ class GroupParticipantController {
     const now = new Date();
 
     try {
-      // 0. 그룹 정보, 참여자 정보, 유저 정보 조회
+      // 1. 그룹 정보, 참여자 정보, 유저 정보 조회
       const [group, participant] = await Promise.all([findGroupById(groupId), findParticipant(nickname, groupId)]);
       let user = await findUserByNickname(nickname);
 
-      // 1. 그룹 존재 여부 확인
+      // 2. 그룹 존재 여부 확인
       if (!group) return handleError(res, null, ERROR_MSG.GROUP_NOT_FOUND, STATUS_CODE.BAD_REQUEST);
-      // 2. 사용자 확인
-      if (user && password !== user.password) return handleError(res, null, ERROR_MSG.PASSWORD_MISMATCH, STATUS_CODE.UNAUTHORIZED);
       // 3. 그룹 내 참여자 존재 조회
-      if (participant) return handleError(res, null, ERROR_MSG.PARTICIPANT_ALREADY_EXISTS, STATUS_CODE.BAD_REQUEST);
+      if (participant) return handleError(res, null, ERROR_MSG.NICKNAME_ALREADY_EXISTS, STATUS_CODE.BAD_REQUEST);
+      // 4. 유저가 있으면 비밀번호 확인
+      if (user && isMatchPassword) return handleError(res, null, ERROR_MSG.PASSWORD_MISMATCH, STATUS_CODE.UNAUTHORIZED);
 
-      // 4. 참여자 생성
+      // 5. 참여자 생성
       await db.$transaction(async (tx) => {
-        // 4-1. 유저가 없으면 생성
+        // 5-1. 유저가 없으면 생성
         if (!user) {
           user = await tx.user.create({
             data: {
@@ -54,13 +53,13 @@ class GroupParticipantController {
             },
           });
         }
-        // 4-2. 참여자 생성
+        // 5-2. 참여자 생성
         await tx.participant.create({
           data: {
-            userId: userObj.id,
+            userId: user.id,
             groupId,
-            createdAt: now,
-            updatedAt: now,
+            createdAt: user.createdAt,
+            updatedAt: user.updatedAt,
           },
           include: {
             user: {
@@ -72,17 +71,18 @@ class GroupParticipantController {
         });
       });
 
-      // 5. 그룹 정보 업데이트
+      // 6. 그룹 정보 업데이트
       const updatedGroup = await db.group.findUnique({
         where: { id: groupId },
         include: {
           owner: true,
           tag: true,
           participants: { include: { user: true } },
+          groupRecommend: true,
         },
       });
 
-      // 6. 새로운 참여자 정보 결과값 가공
+      // 7. 새로운 참여자 정보 결과값 가공
       const response = {
         id: updatedGroup.id,
         name: updatedGroup.name,
@@ -110,7 +110,7 @@ class GroupParticipantController {
         badges: (updatedGroup.badge || []).map(b => b.name),
       };
 
-      res.status(STATUS_CODE.CREATED).json({ ...response });
+      return res.status(STATUS_CODE.CREATED).json({ ...response });
     } catch (error) {
       return handleError(res, error, ERROR_MSG.SERVER_ERROR, STATUS_CODE.INTERNAL_SERVER_ERROR);
     }
@@ -134,34 +134,27 @@ class GroupParticipantController {
         findGroupById(groupId),
       ]);
       const participant = await findParticipant(nickname, groupId);
-      // 0. 유저 조회
-      if (!user) {
-        return handleError(res, null, ERROR_MSG.USER_NOT_FOUND, STATUS_CODE.NOT_FOUND);
-      }
-      // 1. 그룹 조회
-      if (!group) {
-        return handleError(res, null, ERROR_MSG.GROUP_NOT_FOUND, STATUS_CODE.NOT_FOUND);
-      }
-      // 2. 그룹 내 참여자 조회
-      if (!participant) {
-        return handleError(res, null, ERROR_MSG.PARTICIPANT_NOT_FOUND, STATUS_CODE.NOT_FOUND);
-      }
-      // 3. 비밀번호 확인
-      if (!user || user.password !== password) {
-        return handleError(res, null, ERROR_MSG.PASSWORD_MISMATCH, STATUS_CODE.UNAUTHORIZED);
-      }
-      // 해당 유저의 운동 기록 id 목록 조회
+      // 1. 유저 조회
+      if (!user) return handleError(res, null, ERROR_MSG.USER_NOT_FOUND, STATUS_CODE.NOT_FOUND);
+      // 2. 그룹 조회
+      if (!group) return handleError(res, null, ERROR_MSG.GROUP_NOT_FOUND, STATUS_CODE.NOT_FOUND);
+      // 3. 그룹 내 참여자 조회
+      if (!participant) return handleError(res, null, ERROR_MSG.PARTICIPANT_NOT_FOUND, STATUS_CODE.NOT_FOUND);
+      // 4. 비밀번호 확인
+      if (!user || user.password !== password) return handleError(res, null, ERROR_MSG.PASSWORD_MISMATCH, STATUS_CODE.UNAUTHORIZED);
+
+      // 5. 해당 유저의 운동 기록 id 목록 조회
       const exerciseRecordIds = (await db.exerciseRecord.findMany({
         where: { userId: participant.id },
         select: { id: true },
       })).map((record) => record.id);
 
       await db.$transaction(async (tx) => {
-        // 5. 운동 사진 삭제
+        // 6. 운동 사진 삭제
         await tx.photo.deleteMany({ where: { exerciseRecordId: { in: exerciseRecordIds }, } });
-        // 6. 운동 기록 삭제
+        // 7. 운동 기록 삭제
         await tx.exerciseRecord.deleteMany({ where: { userId: participant.id }, });
-        // 4. 참여자 삭제
+        // 8. 참여자 삭제
         await tx.participant.delete({ where: { id: participant.id } });
       });
 
