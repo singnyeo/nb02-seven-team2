0ccd1d20390e02dffb4cc1e2b295b7afd09e65f3
jrkgus413 <jrkgus413@naver.com>
2025-06-19 17:37:41 +0900

    refactor: prisma로 데이터베이스 접근 방식 변경 및 에러 처리 개선

--- backend/middlewares/validation-check.js
+++ backend/middlewares/validation-check.js
@@ -1,27 +1,38 @@
 const { ERROR_MSG, STATUS_CODE } = require('../utils/const');
-const handleError = require('../utils/error');
 
-const groupDataValidation = (req, res, next) => {
-  const groupId = Number(req.params.groupId);
-  const { nickname, password } = req.body;
+class ValidationCheck {
+  static validateGroupId(req, res, next) {
+    const groupId = Number(req.params.groupId);
+    if (Number.isNaN(groupId) || groupId <= 0) {
+      return handleError(next, null, ERROR_MSG.INVALID_GROUP_ID, STATUS_CODE.NOT_FOUND);
+    }
+    return next();
+  }
 
-  // 1. groupId가 숫자인지 확인
-  if (Number.isNaN(groupId) || groupId <= 0) {
-    return handleError(res, null, ERROR_MSG.INVALID_GROUP_ID, STATUS_CODE.NOT_FOUND);
+  static validateNickname(req, res, next) {
+    const { nickname } = req.body;
+    if (typeof nickname !== 'string' || nickname.trim() === '') {
+      return handleError(next, null, ERROR_MSG.INVALID_NICKNAME, STATUS_CODE.NOT_FOUND);
+    }
+    return next();
   }
-  // 2. 닉네임이 문자열인지 확인
-  if (typeof nickname !== 'string' || nickname.trim() === '') {
-    return handleError(res, null, ERROR_MSG.INVALID_NICKNAME, STATUS_CODE.NOT_FOUND);
-  }
-  // 3. 비밀번호가 문자열인지 확인
-  if (typeof password !== 'string' || password.trim() === '') {
-    return handleError(res, null, ERROR_MSG.INVALID_PASSWORD, STATUS_CODE.NOT_FOUND);
-  }
-  // 4. 비밀번호 길이 확인
-  if (process.env.NODE_ENV === 'production' && password.length < 8) {
-    return handleError(res, null, ERROR_MSG.PASSWORD_TOO_SHORT, STATUS_CODE.NOT_FOUND);
-  }
-  return next();
-};
 
-module.exports = groupDataValidation;
+  static validatePassword(req, res, next) {
+    const { password } = req.body;
+    if (typeof password !== 'string' || password.trim() === '') {
+      return handleError(next, null, ERROR_MSG.INVALID_PASSWORD, STATUS_CODE.NOT_FOUND);
+    }
+    return next();
+  }
+
+  static checkPasswordLength(req, res, next) {
+    const { password } = req.body;
+    if (process.env.NODE_ENV === 'production' && password.length < 8) {
+      return handleError(next, null, ERROR_MSG.PASSWORD_TOO_SHORT, STATUS_CODE.NOT_FOUND);
+    }
+    return next();
+  }
+
+}
+
+module.exports = ValidationCheck;
--- backend/utils/error.js
+++ backend/utils/error.js
@@ -3,13 +3,15 @@ const { ERROR_MSG, STATUS_CODE } = require('./const');
 
 // 공통 에러 응답 함수
 const handleError = (
-  res,
+  next,
   error,
   message = ERROR_MSG.SERVER_ERROR,
   status = STATUS_CODE.INTERNAL_SERVER_ERROR,
 ) => {
-  console.error(message, error);
-  res.status(status).json({ error: message });
+  console.log('Error:', error);
+  const err = new Error(message);
+  err.status = status;
+  return next(error);
 };
 
 module.exports = handleError;
